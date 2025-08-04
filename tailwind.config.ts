eb65e1a33bbe5a9b3b54a3a5a9d9c1e5cfc335e6
DjwackoCdmx <djwacko@outlook.es>
2025-08-03 18:33:02 -0600

    fix(tailwind): Correct content paths for production build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,11 +4,9 @@ import tailwindcssAnimate from "tailwindcss-animate";
 export default {
 	darkMode: ["class"],
 	content: [
-		"./pages/**/*.{ts,tsx}",
-		"./components/**/*.{ts,tsx}",
-		"./app/**/*.{ts,tsx}",
-		"./src/**/*.{ts,tsx}",
-	],
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
 	prefix: "",
 	theme: {
 		container: {
